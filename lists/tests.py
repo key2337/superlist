e36599dfd244cd8176d49e9d2f669657a8cde878
key2337 <3526313418@qq.com>
2026-04-27 08:40:20 +0800

    message 8: before refactoring the functional_tests

--- lists/tests.py
+++ lists/tests.py
@@ -6,8 +6,15 @@ from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-    def test_home_page_template_uses_correct_template(self):
+    def test_home_page_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+
 
 # Create your tests here.
