4e57534cd98ecadec937cb322e8794d40343b1b9
key2337 <3526313418@qq.com>
2026-06-01 15:37:41 +0800

    message 15: new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,12 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
-    
-    # 统一改成复数形式 items
+    return render(request, 'home.html')
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-new-page/')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
